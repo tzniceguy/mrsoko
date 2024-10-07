72b166f5a26717f39c0a1639a77f7ff0188f9168
tzniceguy <joeshimbi@gmail.com>
2024-10-07 16:28:21 +0300

    hero section component

---
+++ app/components/Hero.tsx
@@ -0,0 +1,25 @@
+export default function Hero() {
+  return (
+    <div
+      className="bg-no-repeat"
+      style={{ backgroundImage: "url('/hero-section-unsplash.jpg')" }}
+    >
+      <div className="p-6 md:pl-12 h-screen">
+        <div className="box-border justify-center items-center my-36">
+          <h2 className="text-2xl md:text-5xl font-semibold">
+            Too busy to shop?
+            <br />
+            Let us deliver it to your Door!
+          </h2>
+          <p className="text-lg">
+            let us take care of your grocery shopping while you focus on what
+            matters most
+          </p>
+          <button className="bg-orange-500 text-black px-4 py-2 rounded-lg my-8">
+            Follow our whatsapp channel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <main className="p-4 min-h-screen">
+    <main className="p-4 min-h-screen flex flex-col gap-4">
       <NavBar />
+      <Hero />
     </main>
   );
 }
