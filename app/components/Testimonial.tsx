8e8ee1aa40839086a6f6b8b6ecf7046f26cf5b76
tzniceguy <joeshimbi@gmail.com>
2024-10-11 13:35:20 +0300

    added avatar icon for testimonials

--- app/components/Testimonial.tsx
+++ app/components/Testimonial.tsx
@@ -4,32 +4,39 @@ interface TestimonialProps {
   name: string;
   rating: number;
   testimonial: string;
+  image: string;
 }
 
 export default function Testimonial({
   name,
   rating,
+  image,
   testimonial,
 }: TestimonialProps) {
   return (
-    <div className="rounded border-2 border-cyan-100 p-2 h-full">
-      <div className="flex border-2">
+    <div className="rounded-lg border border-sky-100 p-4 shadow-lg bg-white my-2 h-full">
+      <div className="flex items-center border-b border-sky-100 pb-4 mb-4">
         <div className="p-2">
           <Image
-            src="/images/testimonial.jpg"
+            src={image}
             alt="testimonial"
             width={65}
             height={65}
-            className="rounded-full border-2"
+            className="rounded-full border-2 border-sky-100"
           />
         </div>
-        <div className="text-center ">
-          <span className="text-center block">{name}</span>
-          <span>{rating}</span>
+        <div className="ml-4">
+          <span className="block text-lg font-semibold text-gray-700">
+            {name}
+          </span>
+          <span className="block text-yellow-500">
+            {"⭐️".repeat(rating)}{" "}
+            <span className="text-gray-500">({rating}/5)</span>
+          </span>
         </div>
       </div>
       <div>
-        <p className="leading-6">{testimonial}</p>
+        <p className="text-gray-700 leading-6">{testimonial}</p>
       </div>
     </div>
   );
--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -1,15 +1,52 @@
 import Testimonial from "./Testimonial";
 
+interface TestimonialProps {
+  name: string;
+  rating: number;
+  testimonial: string;
+  image: string;
+}
+
+const testimonials: TestimonialProps[] = [
+  {
+    name: "Jane Doe",
+    rating: 4,
+    testimonial:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.",
+    image: "/avatar-female-svgrepo-com.svg",
+  },
+  {
+    name: "John Doe",
+    rating: 4,
+    testimonial:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.",
+    image: "/avatar-male-svgrepo-com .svg",
+  },
+  {
+    name: "John Doe",
+    rating: 4,
+    testimonial:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo.",
+    image: "/avatar-male-svgrepo-com .svg",
+  },
+];
+
 export default function Testimonials() {
   return (
-    <div className="h-screen p-6">
+    <div className="h-full p-6">
       <h1 className="text-2xl md:text-4xl">
         We have served others, are you next?
       </h1>
       <div className="md:grid md:grid-cols-3 gap-3">
-        <Testimonial />
-        <Testimonial />
-        <Testimonial />
+        {testimonials.map((testimonial) => (
+          <Testimonial
+            key={testimonial.name}
+            name={testimonial.name}
+            rating={testimonial.rating}
+            testimonial={testimonial.testimonial}
+            image={testimonial.image}
+          />
+        ))}
       </div>
     </div>
   );
